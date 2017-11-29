9e1f93a424343ebbf068e99e7a40ae9fca1c1e04
Pavel <p-hash@yandex.ru>
2017-11-29 06:38:36 +0300

    Telegram message dumper

---
+++ tgdump/README.md
@@ -0,0 +1,11 @@
+## TG Dump
+
+Downloads all messages from specific user from specific chat into pickle-encoded file.
+
+### Usage
+
+Copy `.env.example` into `.env` and edit it.
+Then run:
+```
+env $(cat .env | xargs) ./dump.py
+```
---
+++ tgdump/dump.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+from getpass import getpass
+from os import environ
+import pickle
+
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.messages import SearchRequest
+from telethon.tl.types import InputMessagesFilterEmpty
+
+if __name__ == '__main__':
+    session_name = environ.get('TG_SESSION', 'session')
+    user_phone = environ['TG_PHONE']
+    client = TelegramClient(
+        session_name, int(environ['TG_API_ID']), environ['TG_API_HASH'],
+        proxy=None, update_workers=4
+    )
+    try:
+        print('INFO: Connecting to Telegram Servers...', end='', flush=True)
+        client.connect()
+        print('Done!')
+
+        if not client.is_user_authorized():
+            print('INFO: Unauthorized user')
+            client.send_code_request(user_phone)
+            code_ok = False
+            while not code_ok:
+                code = input('Enter the auth code: ')
+                try:
+                    code_ok = client.sign_in(user_phone, code)
+                except SessionPasswordNeededError:
+                    password = getpass('Two step verification enabled. '
+                                       'Please enter your password: ')
+                    code_ok = client.sign_in(password=password)
+        print('INFO: Client initialized successfully!')
+
+        target_user = client.get_entity(environ['TGDUMP_USER'])
+        target_chat = client.get_entity(environ['TGDUMP_CHAT'])
+
+        offset = 0
+        all_msgs = []
+        limit = 100
+
+        while True:
+          result = client(SearchRequest(
+            target_chat,
+            '',  # query
+            InputMessagesFilterEmpty(),  # msg_filter
+            None, None,  # min_date, max_date
+            0,  # offset_id
+            offset,  # add_offset
+            limit,  # limit
+            0,  # max_id
+            0,  # min_id
+            target_user
+          ))
+          all_msgs.extend(result.messages)
+          offset += limit
+          if not result.messages:
+            break
+
+        all_msgs = [m.to_dict() for m in all_msgs]
+        filename = 'data/' + environ['TGDUMP_CHAT'] + '.dump'
+        with open(filename, 'wb') as f:
+          pickle.dump(all_msgs, f, pickle.HIGHEST_PROTOCOL)
+          print(str(len(all_msgs)) + ' messages are saved to ' + filename)
+
+        # input('Press Enter to stop this!\n')
+    except KeyboardInterrupt:
+        pass
+    finally:
+        client.disconnect()
+
